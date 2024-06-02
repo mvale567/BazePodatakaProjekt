c203f4ac3359018cb86b6b468a6e4acdce699048
Marko Valecic <marko.valecic@gmail.com>
2024-06-02 21:54:32 +0200

    Update SQL UPITI - Marko.sql

--- SQL UPITI - Marko.sql
+++ SQL UPITI - Marko.sql
@@ -27,103 +27,18 @@ JOIN rezervacija ON gost.id = rezervacija.id_gost
 JOIN racun ON rezervacija.id_racun = racun.id
 GROUP BY gost.ime, gost.prezime;
 
+-- 4. UPIT: Pregled svih gostiju zajedno s njihovim rezervacijama i ukupnim iznosom računa--
 
-SELECT * FROM gost;
+CREATE VIEW gost_rezervacija_racun AS
+SELECT g.ime, g.prezime, g.email, r.datum_prijave, r.datum_odjave, r.broj_gostiju, rc.iznos
+FROM gost AS g
+JOIN rezervacija AS r ON g.id = r.id_gost
+JOIN racun AS rc ON r.id_racun = rc.id;
 
+-- Korištenje pogleda za prikaz podataka
+SELECT * FROM gost_rezervacija_racun;
 
-SELECT radnik.ime, radnik.prezime, radno_mjesto.naziv
-FROM radnik
-JOIN radno_mjesto ON radnik.id_radno_mjesto = radno_mjesto.id;
-
-SELECT gost.id, gost.ime, gost.prezime, SUM(racun.iznos) AS ukupan_racun
-FROM gost
-JOIN rezervacija ON gost.id = rezervacija.id_gost
-JOIN racun ON rezervacija.id_racun = racun.id
-GROUP BY gost.id, gost.ime, gost.prezime;
-
-
-SELECT rezervacija.id_gost, rezervacija.datum_prijave, rezervacija.datum_odjave, 
-       vrsta_placanja.nacin_placanja, SUM(racun.iznos) AS ukupan_iznos
-FROM rezervacija
-JOIN racun ON rezervacija.id_racun = racun.id
-JOIN racun_vrsta_placanja ON racun.id = racun_vrsta_placanja.id_racun
-JOIN vrsta_placanja ON racun_vrsta_placanja.id_placanje = vrsta_placanja.id
-GROUP BY rezervacija.id_gost, rezervacija.datum_prijave, rezervacija.datum_odjave, vrsta_placanja.nacin_placanja;
-
-SELECT rezervacija.id_gost, rezervacija.datum_prijave, rezervacija.datum_odjave, 
-       soba.broj_sobe, vrsta_placanja.nacin_placanja, SUM(racun.iznos) AS ukupan_iznos
-FROM rezervacija
-JOIN racun ON rezervacija.id_racun = racun.id
-JOIN racun_vrsta_placanja ON racun.id = racun_vrsta_placanja.id_racun
-JOIN vrsta_placanja ON racun_vrsta_placanja.id_placanje = vrsta_placanja.id
-JOIN soba ON rezervacija.id = soba.id_raspored_ciscenja  -- Promijenjen spoj na ID rezervacije
-GROUP BY rezervacija.id_gost, rezervacija.datum_prijave, rezervacija.datum_odjave, soba.broj_sobe, vrsta_placanja.nacin_placanja;
-
-
-SELECT gost.ime, gost.prezime, rezervacija.datum_prijave, rezervacija.datum_odjave, 
-       soba.broj_sobe, vrsta_placanja.nacin_placanja, SUM(racun.iznos) AS ukupan_iznos
-FROM rezervacija
-JOIN racun ON rezervacija.id_racun = racun.id
-JOIN racun_vrsta_placanja ON racun.id = racun_vrsta_placanja.id_racun
-JOIN vrsta_placanja ON racun_vrsta_placanja.id_placanje = vrsta_placanja.id
-JOIN soba ON rezervacija.id = soba.id_raspored_ciscenja
-JOIN gost ON rezervacija.id_gost = gost.id
-GROUP BY gost.ime, gost.prezime, rezervacija.datum_prijave, rezervacija.datum_odjave, soba.broj_sobe, vrsta_placanja.nacin_placanja;
-
-
-
-
-SELECT rc.id AS 'ID rasporeda čišćenja', rc.datum AS 'Datum čišćenja', rc.stanje AS 'Stanje čišćenja',
-       s.id AS 'ID sobe', s.broj_sobe AS 'Broj sobe', s.tip AS 'Tip sobe',
-       r.id AS 'ID radnika', CONCAT(r.ime, ' ', r.prezime) AS 'Ime i prezime radnika', rm.naziv AS 'Radno mjesto'
-FROM raspored_ciscenja rc
-INNER JOIN soba s ON rc.id = s.id_raspored_ciscenja
-LEFT JOIN radnik_raspored_ciscenja rrc ON rc.id = rrc.id_raspored_ciscenja
-LEFT JOIN radnik r ON rrc.id_radnik = r.id
-LEFT JOIN radno_mjesto rm ON r.id_radno_mjesto = rm.id 
-WHERE rm.naziv = 'Čistać';
-
-
-
-
-
-
-SELECT rc.datum, CONCAT(r.ime, ' ', r.prezime) AS radnik, s.broj_sobe
-FROM raspored_ciscenja rc
-JOIN radnik_raspored_ciscenja rrc ON rc.id = rrc.id_raspored_ciscenja
-JOIN radnik r ON rrc.id_radnik = r.id
-JOIN soba s ON rc.id_soba = s.id
-WHERE r.odjel = 'Cistaci';
-
-SELECT radnik.id, raspored_ciscenja.id
-FROM radnik
-JOIN radno_mjesto ON radnik.id_radno_mjesto = radno_mjesto.id
-JOIN raspored_ciscenja ON radno_mjesto.odjel = 'Odrzavanje';
-
-
-
-
-
-
-CREATE VIEW prosjecni_iznos_racuna_restoran AS
-SELECT AVG(cijena) AS prosjecni_iznos
-FROM racun_restoran;
-
-SELECT * FROM prosjecni_iznos_racuna_restoran;
-
-
-
-SELECT s.broj_sobe, AVG(r.ocjena) AS prosjecna_ocjena
-FROM soba AS s
-JOIN recenzija AS r ON s.id = r.id_rezervacija
-GROUP BY s.broj_sobe
-ORDER BY prosjecna_ocjena DESC
-LIMIT 5;
-
-
-
-
-
+-- 5. UPIT
 
 SELECT 
     g.id AS 'ID Gosta',
@@ -145,3 +60,5 @@ LEFT JOIN
     usluge AS u ON ru.id_usluga = u.id;
 
 SELECT * FROM skladiste_dobavljac;
+
+
