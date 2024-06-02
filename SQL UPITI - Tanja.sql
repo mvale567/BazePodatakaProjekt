0776161504cf6d4b503b3a00b79662824234cb6f
Taniko-coder <121758792+Taniko-coder@users.noreply.github.com>
2024-06-02 17:29:38 +0200

    Update SQL UPITI - Tanja.sql

--- SQL UPITI - Tanja.sql
+++ SQL UPITI - Tanja.sql
@@ -22,6 +22,15 @@ WHERE stanje='Nije završeno';
 
 -- KRAJ UPITA 2
 
+-- UPIT 3
+
+SELECT r.ime, r.prezime, s.naziv AS smjena
+FROM radnik r
+JOIN radnik_smjena_radnika rs ON r.id = rs.id_radnik
+JOIN smjena_radnika s ON rs.id_smjena = s.id;
+
+-- KRAJ UPITA 2
+
 /*Šef recepcije zatražio je evidenciju je li očišćena soba 101. 
 To je napravljeno na sljedeći način: 
 Koristimo atribute iz tablica soba i raspored_ciscenja kako bi kreirali pogled pregled_soba_ciscenja koji sadrži podatke o broju sobe, datumu i stanju čišćenja. Korištenjem operatora INNER JOIN, CREATE VIEW, SELECT i WHERE, upit omogućava filtriranje i dohvaćanje specifičnih informacija, poput provjere je li soba 101 očišćena.
@@ -65,3 +74,16 @@ To nam omogućuje dohvaćanje samo onih soba koje nisu očišćene, odnosno čij
 
 --Kraj upita 2
 
+/*Upit 3 – Hotelski manager zatražio je evidenciju koji radnik radi u kojoj smjeni. To je napravljeno na sljedeći način: Da bi saznali koji radnik je u kojoj smjeni, trebamo spojiti tablice radnik i smjena_radnika koristeći tablicu koja povezuje radnike sa smjenama, tj. radnik_smjena_radnika. To se ostvaruje sa sljedećim SQL upitom:
+
+SELECT r.ime, r.prezime, s.naziv AS smjena
+FROM radnik r
+JOIN radnik_smjena_radnika rs ON r.id = rs.id_radnik
+JOIN smjena_radnika s ON rs.id_smjena = s.id;
+
+Koristimo atribute r.ime za ime radnika, r.prezime za prezime radnika, s.naziv AS smjena za naziv smjene iz tablice smjena_radnika, koji se preimenuje u smjena. AS ključna riječ koristi se za preimenovanje stupca u rezultatu kao "smjena".
+Operator SELECT bira specifične stupce koje želimo prikazati - atribute r.ime, r.prezime i s.naziv AS smjena.
+Operator FROM identificira tablice iz kojih se podaci dohvaćaju,  to su radnik r koja sadrži informacije o radnicima, kao što su ime i prezime.
+Operator JOIN kombinira redove iz više tablica prema određenom uvjetu - spaja tablice radnik i radnik_smjena_radnika prema id-u radnika, zatim spaja tablice radnik_smjena_radnika i smjena_radnika prema id_smjena.
+       
+Ovi atributi i operatori zajedno tvore SQL upit koji dohvaća informacije o radnicima i smjenama koje rade. Spajanjem tablica koristeći ključeve kao što su id_radnik i id_smjena, omogućuje se dobivanje kompletnih informacija o radnicima i njihovim smjenama.
